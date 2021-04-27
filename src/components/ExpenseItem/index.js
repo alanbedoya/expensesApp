3078cdf90c67f8e89c413d1c7fe6b477efb346b4
Alan Bedoya <alanjbedoya@gmail.com>
2021-04-27 01:38:16 -0700

    Adding styles and cleaning up folder structure.

--- src/components/ExpenseItem/index.js
+++ src/components/ExpenseItem/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './ExpenseItem.css';
+import { ExpenseDate, Card } from '../../components';
 
 export const ExpenseItem = (props) => {
   return (
-    <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
 export * from './ExpenseItem';
+export * from './ExpenseDate';
+export * from './Expenses';
+export * from './Card';
